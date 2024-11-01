948aa911a8a6f32f32e6d82d4524a91e5f03e3a6
heronfelipe10 <herongamer0610@gmail.com>
2024-10-31 23:49:59 -0300

    alteracoes front

--- src/app/components/pages/principal/principal.component.ts
+++ src/app/components/pages/principal/principal.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../../side-bar/side-bar.component';
 import { RouterOutlet } from '@angular/router';
-import { SearchBarComponent } from "../../search-bar/search-bar.component";
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
