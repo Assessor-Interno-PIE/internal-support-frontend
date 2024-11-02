0c690b3e6a74e4dba617c36e83338857fdd66ae0
João Pedro <joao.pcanhete@gmail.com>
2024-11-02 02:07:02 -0300

    alteração no layout do login - Css - Ts

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -7,19 +7,35 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  username: string = 'admin'; // Definindo um valor padrão
-  password: string = 'admin'; // Definindo um valor padrão
+  @ViewChild('container') container!: ElementRef;
 
-  router = inject(Router);
+  email: string = 'admin';
+  password: string = 'admin';
 
-  constructor(){
+  constructor(private renderer: Renderer2, private router: Router) {}
+
+  toggleContainer(action: 'register' | 'login') {
+    // Verifica se o container está definido antes de acessar seu nativeElement
+    if (this.container) {
+      const containerElement = this.container.nativeElement;
+
+      if (action === 'register') {
+        this.renderer.addClass(containerElement, 'active');
+        console.log("Classe 'active' adicionada para 'register'");
+      } else if (action === 'login') {
+        this.renderer.removeClass(containerElement, 'active');
+        console.log("Classe 'active' removida para 'login'");
+      }
+    } else {
+      console.error("Container não está definido.");
+    }
   }
 
   onSubmit() {
-    if (this.username === 'admin' && this.password === 'admin') {
+    if (this.email === 'admin' && this.password === 'admin') {
       this.router.navigate(['admin/dashboard']);
     } else {
       alert('Login inválido.');
