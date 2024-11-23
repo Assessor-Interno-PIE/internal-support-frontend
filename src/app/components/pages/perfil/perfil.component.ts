c791cf3409a07373f2430d7cd9db76a42b2db7ff
João Graciotim <graciotim92@gmail.com>
2024-11-23 16:26:00 -0300

    exibição de documentos por departamento do usuário na dasboard

--- src/app/components/pages/perfil/perfil.component.ts
+++ src/app/components/pages/perfil/perfil.component.ts
@@ -11,8 +11,8 @@ export class PerfilComponent {
   usuario = {
     nome: 'João Pedro Canhete',
     email: 'joaopedro.profissional@gmail.com',
-    biografia: 'Desenvolvedor de software Full Stack.',
+    biografia: 'Designer',
     foto: 'https://media-gru2-2.cdn.whatsapp.net/v/t61.24694-24/436491333_983772643752473_9036282596056724518_n.jpg?ccb=11-4&oh=01_Q5AaIDPs-ohN_J0vvDT1V8FKf_aAVU2CBaAQoEZuvVF9tTLL&oe=6738A9B3&_nc_sid=5e03e0&_nc_cat=111',
-    departamento: 'Desenvolvimento',
+    departamento: 'Marketing',
   };
 }
